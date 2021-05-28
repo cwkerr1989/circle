b9f6c153ff59406f1b12a9ddda95ddff379aa728
Warren <cwkerr1989@gmail.com>
2021-05-27 23:39:57 -0400

    Added circumference calc and corrected area calc.

--- circle.cpp
+++ circle.cpp
@@ -10,6 +10,8 @@ int main()
     cout << "Enter radius\n";
     cin >> radius;
     cout << "The area of a circle with radius " << radius << " is: ";
-    cout << 2 * pi * radius;
+    cout << pow((pi * radius), 2) << endl;
+    cout << "The circumference of a circle with radius " << radius << " is: " << 2 * pi * radius;
+
     return 0;
 }
